5d5eadcd1cbc416907992bd18378e0c3edf0438d
Ares Velegrakis <arisveleg@gmail.com>
2025-03-23 17:18:06 +0200

    resources initialization / cleanup

--- retrievvy/__main__.py
+++ retrievvy/__main__.py
@@ -1,16 +1,23 @@
 import sys
+import atexit
 
 from loguru import logger
 from retrievvy import database, webserver
+from retrievvy.nlp import embeddings
 
 if __name__ == "__main__":
     from retrievvy import config  # has side-effects
 
-    # Initialize the database
+    # Initialize resources
     database.init()
+    embeddings.start_worker()
 
     if config.DEBUG:
         logger.remove()
         logger.add(sys.stderr, level="DEBUG")
 
+    # Register resource cleanup funcs
+    atexit.register(embeddings.shutdown_worker)
+
+    # Start the webserver
     webserver.run()
